980b9f0b60c570f867d1b1040ad2bf818d6c3183
Duda Oleksandr <duda.oleks@gmail.com>
2021-08-11 11:54:11 +0300

    Init of new branch

--- HttpTriggerToFileUpload/FileTriggerFunction.cs
+++ HttpTriggerToFileUpload/FileTriggerFunction.cs
@@ -9,7 +9,7 @@ namespace HttpTriggerToFileUpload
         [FunctionName("FileTriggerFunction")]
         public static void Run([BlobTrigger("file-storage/{name}", Connection = "ConnectionString")]Stream myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes \n");
         }
     }
 }
