4d0ea0fb82a7120619c4285462def4cbad24be7b
Duda Oleksandr <duda.oleks@gmail.com>
2021-08-13 15:07:45 +0300

    Error handling added

--- FileUploadFunctions/FileTriggerFunction.cs
+++ FileUploadFunctions/FileTriggerFunction.cs
@@ -1,4 +1,6 @@
-﻿using FileUploadMonitor.Core.Services;
+﻿using System;
+using Common.Exceptions;
+using FileUploadMonitor.Core.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -20,9 +22,28 @@ namespace FileUploadFunctions
             [BlobTrigger("file-storage/{name}", Connection = "ConnectionString")] string  myBlob, string name,
             FunctionContext context)
         {
-            var res = _fileUploadService.UploadFile(myBlob, name);
             var logger = context.GetLogger("FileTriggerFunction");
-            logger.LogInformation(JsonConvert.SerializeObject(res, Formatting.Indented));
+
+            try
+            {
+                var res = _fileUploadService.UploadFile(myBlob, name);
+                logger.LogInformation(JsonConvert.SerializeObject(res, Formatting.Indented));
+            }
+            catch (ValidationAggregationException validationAggregationException)
+            {
+                logger.LogInformation(JsonConvert.SerializeObject(validationAggregationException.Message, Formatting.Indented));
+
+                foreach (var error in validationAggregationException.Exceptions)
+                {
+                    logger.LogInformation(JsonConvert.SerializeObject(new {error.Message, error.Source}, Formatting.Indented));
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                logger.LogInformation(JsonConvert.SerializeObject(new {ex.Message, ex.Source }, Formatting.Indented));
+            }
+            
         }
     }
 }
